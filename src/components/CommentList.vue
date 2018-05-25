e44d3fe9a6a0fa850872ede444c18706d82252dc
SynergixeNG <10416482+synergixe@users.noreply.github.com>
2018-05-25 15:46:19 +0100

    Create CommentList.vue

---
+++ src/components/CommentList.vue
@@ -0,0 +1,20 @@
+<template>
+    <ul class="comment-box_list">
+        <comment v-for="comment in comments" :author="comment.author" :text="comment.text"></comment>
+    </ul>
+</template>
+
+<script>
+    import Comment from "./Comment.vue";
+    
+    export default {
+        components: { Comment },
+        props: {
+                  comments:Array
+        }
+    };
+</script>
+
+<style>
+   
+</style>
