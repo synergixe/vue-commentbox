c96fe8d67072c1208601d632eebaaeb62e0166d6
SynergixeNG <10416482+synergixe@users.noreply.github.com>
2018-05-25 15:39:48 +0100

    Create Comment.vue

---
+++ src/components/Comment.vue
@@ -0,0 +1,16 @@
+<template>
+  <li class="comment">
+      <!--<span class="comment_author">{{ author }}</span>
+      <span class="comment_text">{{ text }}</span>-->
+  </li>
+</template>
+
+<script>
+    export default {
+        props: ["author", "text"]
+    };
+</script>
+
+<style>
+    
+</style>
