06ad0c230c9edcb647f0b3e24e6e9121388c227e
SynergixeNG <10416482+synergixe@users.noreply.github.com>
2018-05-25 15:37:38 +0100

    Create CommentBox.vue

---
+++ src/components/CommentBox.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="comment-box" v-cloak>
+    <h1 v-once=""></h1>
+    <p v-once=""></p>
+    <comment-list :comments="comments"></comment-list>
+    <comment-form></comment-form>
+  </div>
+</template>
+
+<script>
+    import CommentList from "./CommentList.vue";
+    import CommentForm from "./CommentForm.vue";
+    export default {
+        components: { CommentList, CommentForm },
+        data () {
+            return {
+                comments: [
+                    {author: "Mike", text: "This is a comment"},
+                    {author: "Joe", text: "This is another comment"},
+                ]
+            }
+        },
+        events: {
+            // Add new comment
+            "new-comment": function(comment) {
+                this.comments.push(comment);
+            }
+        }
+    }
+</script>
+
+<style>
+    
+</style>
