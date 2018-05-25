20b8f0a79badef9e6959b0638f5b9ba612363ff3
SynergixeNG <10416482+synergixe@users.noreply.github.com>
2018-05-25 16:28:12 +0100

    Update CommentBox.vue

--- src/components/CommentBox.vue
+++ src/components/CommentBox.vue
@@ -3,7 +3,7 @@
     <h1 v-once=""></h1>
     <p v-once=""></p>
     <comment-list :comments="comments"></comment-list>
-    <comment-form></comment-form>
+    <comment-form :author="author" :avartar-thumbnail="avatar-url-thumb"></comment-form>
   </div>
 </template>
 
@@ -12,11 +12,15 @@
     import CommentForm from "./CommentForm.vue";
     export default {
         components: { CommentList, CommentForm },
+        props:{
+          author:String,
+          avatarUrlThumb:String
+        },
         data () {
             return {
                 comments: [
-                    {avatar_thumbnail:"", author: "Mike", text: "This is a comment"},
-                    {avatar_thumbnail:"", author: "Joe", text: "This is another comment"},
+                    {avatar_url_thumb:"", author: "Mike", text: "This is a comment"},
+                    {avatar_url_thumb:"", author: "Joe", text: "This is another comment"},
                 ]
             }
         },
