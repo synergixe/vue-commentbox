cb07b84933e9124f6e296f1937dfa3691d6757e0
SynergixeNG <10416482+synergixe@users.noreply.github.com>
2018-05-25 15:51:28 +0100

    Create CommentForm.vue

---
+++ src/components/CommentForm.vue
@@ -0,0 +1,33 @@
+<template>
+    <form class="comment-form" v-on:submit.prevent="onSubmit">
+        
+    </form>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                text: ""
+            }
+        },
+        methods: {
+            // Handle form submit
+            onSubmit() {
+                if (this.text !== "") {
+                    // Dispatch event to parent components
+                    this.$dispatch("new-comment", {
+                        author: this.author,
+                        text: this.text
+                    });
+                    // Reset Form
+                    this.text = "";
+                }
+            }
+        }
+    };
+</script>
+
+<style>
+    
+</style>
