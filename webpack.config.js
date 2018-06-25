4aa0e271e8ea2415230b6224666218f5e61e6f1f
SynergixeNG <10416482+synergixe@users.noreply.github.com>
2018-06-25 14:23:10 -0500

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
+	mode: process.env.NODE_ENV,
 	entry:{
 		"comment-box":"./src/components/CommentBox.vue",
 		/*"exts":[
