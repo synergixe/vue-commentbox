4b9a618c615a2c7d2ea1265e4db090108f37bc61
SynergixeNG <10416482+synergixe@users.noreply.github.com>
2018-06-25 13:09:06 -0500

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -7,40 +7,20 @@ module.exports = {
 		filename:"./dist/[name].js"
 	},
 	module:{
-		loaders:[
-			{test: /\.vue$/, loader:"vue"},
-			{test: /\.js$/, loader:"babel"}
+		rules: [
+			{
+			    test: /\.vue$/,
+			    exclude: /node_modules/,
+			    loader: 'vue-loader'
+			},
+			{
+			    test: /\.js$/,
+			    exclude: /node_modules/,
+			    use: {
+			    loader: 'babel-loader'
+			    }
+			}
 		]
 	}
 };
 
-
-const { VueLoaderPlugin } = require('vue-loader');
-const path = require('path');
-const env = process.env.NODE_ENV;
-
-const config = {
-  entry: path.join(__dirname, 'src', 'main.js'),
-  mode: env,
-  output: {
-    publicPath: '/',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.vue$/,
-        loader: 'vue-loader',
-      },
-      {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        include: [path.join(__dirname, 'src')],
-      }
-    ],
-  },
-  plugins: [
-    new VueLoaderPlugin()
-  ],
-};
-
-module.exports = config;
