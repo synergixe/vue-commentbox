8a1d821b837bad7d43f30bc4f06b66d86265ae7d
SynergixeNG <10416482+synergixe@users.noreply.github.com>
2018-06-27 13:48:26 -0500

    Update CommentList.vue

--- src/components/CommentList.vue
+++ src/components/CommentList.vue
@@ -1,16 +1,25 @@
 <template>
     <div class="comment-list-row">
-        <ul class="comment-list">
+        <ul class="comment-list" ref="clist">
             <comment v-for="(comment, index) in comments" :key="`comment-${index}`"> :author="comment.author" :text="comment.text" :avatar-thumbnail="comment.avatar_thumbnail"></comment>
         </ul>
     </div>
 </template>
 
 <script>
+    import * as Helpers from '../utils/helpers'
+
     import Comment from "./Comment.vue";
     
     export default {
         components: { Comment },
+        mounted(){
+
+            if(!Helpers.doesSupportEmoji()){
+	            Helpers.replaceAllEmojiInAncestor(this.$ref.clist);
+            }
+
+        },
         props: {
             comments:Array
         }
