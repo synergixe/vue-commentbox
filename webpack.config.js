0750c7683c394e315e39eab3198f9e1aece26761
SynergixeNG <10416482+synergixe@users.noreply.github.com>
2018-07-10 08:21:50 -0500

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
 	entry:path.resolve(__dirname, '/src/index.js'),
 	output:{
 		path: path.resolve(__dirname, './dist'),
-		library: 'vueCommentBox',
+		library: 'vue-commentbox',
 	    	libraryTarget: 'umd',
 	    	filename:"vueCommentBox.js",
 	    	umdNamedDefine: true
