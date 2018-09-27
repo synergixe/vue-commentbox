2c722eaeb17d429d3b57d5b497b0ab273c41e996
SynergixeNG <10416482+synergixe@users.noreply.github.com>
2018-09-27 14:23:25 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import CommentBox from './CommentBox.vue'
 
 const CommentBoxPlugin = {
-  install: function(Vue, options) {
+  install: function(Vue, options = {}) {
+      if (this.install.installed) return;
+      
       Vue.component(CommentBox.name, CommentBox)
   }
+};
+
+// auto install
+if (typeof window !== 'undefined' && window.Vue) {
+  CommentBoxPlugin.install(window.Vue)
 }
+
 
 export { CommentBox, CommentBoxPlugin }
